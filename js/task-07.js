31af04fee7116dab8de946b0108393e376cb6850
LiliaSlabka <liaslabka@gmail.com>
2023-01-30 17:21:00 +0200

    upgrade task 1

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputEl = document.getElementById('font-size-control')
 const spanText = document.getElementById('text')
 console.log(inputEl)
-
+ 
 inputEl.addEventListener("change", onInputFontSize)
 
 function onInputFontSize(event) {
